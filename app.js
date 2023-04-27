d725a60a2cc387e6e8464bd91be32ad6228117a2
GuerreroEnier <guerreroenier@gmail.com>
2023-04-27 10:32:22 -0500

    3 commit

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.post("/", (req, res) => {
     ],
   };
   const jsonData = JSON.stringify(data);
+
   const url = `https://us14.api.mailchimp.com/3.0/lists/${audiId}`;
   const options = {
     method: "POST",
@@ -43,8 +44,7 @@ app.post("/", (req, res) => {
   const request = https.request(url, options, (response) => {
     response.on("data", (data) => {
       const resJson = JSON.parse(data);
-      
-      if (resJson.new_members && resJson.new_members.length > 0 && response.statusCode === 200) {
+      if (resJson.new_members.length > 0 && response.statusCode === 200) {
         res.sendFile(__dirname + "/success.html");
       } else {
         res.sendFile(__dirname + "/failure.html");
